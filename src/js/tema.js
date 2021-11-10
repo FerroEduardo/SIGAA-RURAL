448b30399074c75ff88bed52ff47bb750fa92322
FerroEduardo <47820549+FerroEduardo@users.noreply.github.com>
2021-11-10 15:03:48 -0300

    var -> let e renomeações

--- src/js/tema.js
+++ src/js/tema.js
@@ -1,19 +1,19 @@
-let generateCSSTheme = function (COR_1, COR_2, COR_3, COR_4) {
+let generateCSSTheme = function (cor1, cor2, cor3, cor4) {
     return `
         .hover-bordered-bottom:hover {
-            border-bottom: 3px solid ${COR_1};
+            border-bottom: 3px solid ${cor1};
         }
         #barra-sistema {
-            background-color: ${COR_1};
+            background-color: ${cor1};
         }
         #barra-sistema .botao-sair a:hover {
-            background-color: ${COR_2};
+            background-color: ${cor2};
         }
         #painel-usuario .logo > span {
-            color: ${COR_1};
+            color: ${cor1};
         }
         #painel-usuario .painel-usuario-identificacao span {
-            color: ${COR_1};
+            color: ${cor1};
         }
         #painel-usuario .painel-usuario-foto {
             display: table-cell;
@@ -23,152 +23,152 @@ let generateCSSTheme = function (COR_1, COR_2, COR_3, COR_4) {
             vertical-align: middle;
         }
         #detalhes-usuario td.detalhes-usuario-nome {
-            color: ${COR_1};
+            color: ${cor1};
         }
         #detalhes-usuario .acoes-usuario li a:hover,
         #detalhes-usuario .acoes-usuario li a:focus {
-            border-bottom: 5px solid ${COR_1};
+            border-bottom: 5px solid ${cor1};
         }
         ul#paleta-cores li.cor10,
         ul#paleta-cores-horizontal li.cor10 {
-            background-color: ${COR_1};
+            background-color: ${cor1};
         }
         #ufrn-rodape {
-            background-color: ${COR_2};
+            background-color: ${cor2};
         }
         #acesso-rapido .carousel-control:hover {
-            color: ${COR_1};
+            color: ${cor1};
         }
         .menu-lateral > ul > li.selecionado {
-            border-left: 4px solid ${COR_1};
+            border-left: 4px solid ${cor1};
         }
         .search input[type="text"]:focus {
             border: none;
-            border-bottom: 0.5px solid ${COR_1};
+            border-bottom: 0.5px solid ${cor1};
         }
         .page-sidebar .sidebar-header-wrapper .searchicon {
-            color: ${COR_1};
+            color: ${cor1};
         }
         .page-sidebar .sidebar-menu li:before {
-            background-color: ${COR_1};
+            background-color: ${cor1};
         }
         .table .acoes a:hover {
-            color: ${COR_1};
+            color: ${cor1};
         }
         .table > tbody > tr.destaque td {
-            background: ${COR_3} !important;
+            background: ${cor3} !important;
         }
         .table > tbody > tr.secao td {
-            border-bottom: 1px solid ${COR_1};
+            border-bottom: 1px solid ${cor1};
         }
         .table > tbody > tr.selecionado td {
-            background: ${COR_3} !important;
+            background: ${cor3} !important;
         }
         .rich-table .acoes a:hover,
         table.listagem .acoes a:hover {
-            color: ${COR_1};
+            color: ${cor1};
         }
         .rich-table > tbody > tr.destaque td,
         table.listagem > tbody > tr.destaque td {
-            background: ${COR_3} !important;
+            background: ${cor3} !important;
         }
         .rich-table > tbody > tr.secao td,
         table.listagem > tbody > tr.secao td {
-            border-bottom: 1px solid ${COR_1};
+            border-bottom: 1px solid ${cor1};
         }
         .rich-table > tbody > tr.selecionado td,
         table.listagem > tbody > tr.selecionado td {
-            background: ${COR_3} !important;
+            background: ${cor3} !important;
         }
         .page-header {
-            color: ${COR_2};
+            color: ${cor2};
         }
         .list-selected .item {
-            background-color: ${COR_3};
+            background-color: ${cor3};
         }
         a {
-            color: ${COR_1};
+            color: ${cor1};
         }
         a:focus,
         a:hover {
-            color: ${COR_2};
+            color: ${cor2};
         }
         a .bordered-hover:hover {
-            border-left: 5px solid ${COR_1};
+            border-left: 5px solid ${cor1};
         }
         .bg-claro {
-            background-color: ${COR_3} !important;
+            background-color: ${cor3} !important;
         }
         #carousel-noticias .box-noticia-conteudo h4 {
-            color: ${COR_1};
+            color: ${cor1};
         }
         .tabela-selecao-vinculo .check-ativo {
-            color: ${COR_1};
+            color: ${cor1};
         }
         .tabela-selecao-vinculo tr.selecionado > td {
-            background-color: ${COR_3} !important;
+            background-color: ${cor3} !important;
         }
         .sm-blue {
-            background: ${COR_1};
+            background: ${cor1};
         }
         .sm-blue ul a.highlighted,
         .sm-blue ul a:active,
         .sm-blue ul a:focus,
         .sm-blue ul a:hover {
-            border-left: 5px solid ${COR_1};
+            border-left: 5px solid ${cor1};
         }
         .fc-unthemed .fc-today {
-            background: ${COR_3};
+            background: ${cor3};
         }
         .fc-toolbar button {
-            color: ${COR_1};
+            color: ${cor1};
         }
         .btn-light:focus,
         .btn-light:hover,
         .btn-primary {
-            background-color: ${COR_1} !important;
-            border-color: ${COR_1} !important;
+            background-color: ${cor1} !important;
+            border-color: ${cor1} !important;
         }
         .btn-primary:focus,
         .btn-primary:hover {
-            background-color: ${COR_2} !important;
-            border-color: ${COR_2} !important;
+            background-color: ${cor2} !important;
+            border-color: ${cor2} !important;
         }
         .progress-bar {
-            background-color: ${COR_2};
-            background-image: linear-gradient(${COR_1} 0, ${COR_2} 100%);
+            background-color: ${cor2};
+            background-image: linear-gradient(${cor1} 0, ${cor2} 100%);
         }
         caption.formulario {
-            background-color: ${COR_1};
+            background-color: ${cor1};
         }
         table.formulario > caption,
         table.listagem > caption,
         table.rich-table.listagem > caption,
         table.subFormulario > caption,
         table.visualizacao > caption {
-            background: ${COR_1};
+            background: ${cor1};
         }
         h3.tituloTabela {
-            background: ${COR_1};
+            background: ${cor1};
         }
         .page-sidebar .sidebar-menu li:not(.open):hover:before {
-            background-color: ${COR_4};
+            background-color: ${cor4};
         }
         .table tr.destaque {
-            background-color: ${COR_4} !important;
+            background-color: ${cor4} !important;
         }
         .rich-table tr.destaque,
         table.listagem tr.destaque {
-            background-color: ${COR_4} !important;
+            background-color: ${cor4} !important;
         }
         .list-selected .item .item-remove {
-            color: ${COR_4};
+            color: ${cor4};
         }
         .btn-light {
-            background-color: ${COR_4};
+            background-color: ${cor4};
         }
         .painel-numerico {
-            background: ${COR_4};
+            background: ${cor4};
         }
         .sm-blue a.current,
         .sm-blue a.current:active,
@@ -178,25 +178,25 @@ let generateCSSTheme = function (COR_1, COR_2, COR_3, COR_4) {
         .sm-blue ul a.current:active,
         .sm-blue ul a.current:focus,
         .sm-blue ul a.current:hover {
-            background: ${COR_4};
+            background: ${cor4};
         }
         .sm-blue a span.sub-arrow {
-            border-color: ${COR_4} transparent transparent;
+            border-color: ${cor4} transparent transparent;
         }
         .sm-blue ul a span.sub-arrow,
         .sm-blue-vertical a span.sub-arrow {
-            border-color: transparent transparent transparent ${COR_4};
+            border-color: transparent transparent transparent ${cor4};
         }
         .sm-blue span.scroll-down-arrow,
         .sm-blue span.scroll-up-arrow {
-            border-color: transparent transparent ${COR_4};
+            border-color: transparent transparent ${cor4};
         }
         .sm-blue span.scroll-down-arrow {
-            border-color: ${COR_4} transparent transparent;
+            border-color: ${cor4} transparent transparent;
         }
         .btn-light {
-            background-color: ${COR_4} !important;
-            border-color: ${COR_4} !important;
+            background-color: ${cor4} !important;
+            border-color: ${cor4} !important;
         }
         caption.subFormulario,
         table caption.subTabela,
@@ -207,7 +207,7 @@ let generateCSSTheme = function (COR_1, COR_2, COR_3, COR_4) {
         table.listagem > tbody > tr.secao > td,
         table.listagem > tbody > tr.secao > th,
         td.subFormulario {
-            border-bottom: 1px solid ${COR_4};
+            border-bottom: 1px solid ${cor4};
         }
     `;
 }
@@ -226,7 +226,7 @@ function generateThemeButton() {
   
   for (let i = 0; i < 4; i++) {
     let colorButton = document.createElement('input');
-    colorButton.id = `COR_${i}`
+    colorButton.id = `cor${i}`
     colorButton.type = 'color';
     colorButton.value = getCurrentColor[i]
     colorButton.style.width = '71px';
@@ -243,10 +243,10 @@ function generateThemeButton() {
   applyButton.onclick = function (e) {
     let themeTextNode = document.createTextNode(
       generateCSSTheme(
-        document.getElementById('COR_0').value, 
-        document.getElementById('COR_1').value, 
-        document.getElementById('COR_2').value, 
-        document.getElementById('COR_3').value
+        document.getElementById('cor0').value, 
+        document.getElementById('cor1').value, 
+        document.getElementById('cor2').value, 
+        document.getElementById('cor3').value
       ));
     let themeStyle = document.createElement('style');
     themeStyle.appendChild(themeTextNode);
@@ -259,14 +259,14 @@ function generateThemeButton() {
 }
   
 function componentFromStr(numStr, percent) {
-  var num = Math.max(0, parseInt(numStr, 10));
+  let num = Math.max(0, parseInt(numStr, 10));
   return percent ?
       Math.floor(255 * Math.min(100, num) / 100) : Math.min(255, num);
 }
   
 function rgbToHex(rgb) {
-  var rgbRegex = /^rgb\(\s*(-?\d+)(%?)\s*,\s*(-?\d+)(%?)\s*,\s*(-?\d+)(%?)\s*\)$/;
-  var result, r, g, b, hex = "";
+  let rgbRegex = /^rgb\(\s*(-?\d+)(%?)\s*,\s*(-?\d+)(%?)\s*,\s*(-?\d+)(%?)\s*\)$/;
+  let result, r, g, b, hex = "";
   if ( (result = rgbRegex.exec(rgb)) ) {
       r = componentFromStr(result[1], result[2]);
       g = componentFromStr(result[3], result[4]);
